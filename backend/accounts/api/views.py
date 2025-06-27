a25e7f9517ff1b42e958fac5bed5c19024fde57e
“G-Gakii” <gakiiviolet1@gmail.com>
2025-06-27 16:18:49 +0300

    Add Replit config and startup script

--- backend/accounts/api/views.py
+++ backend/accounts/api/views.py
@@ -1,11 +1,10 @@
 from accounts.models import Account
 from accounts.api.serializers import AccountSerializers
 from rest_framework import generics
-from rest_framework import filters
+
 
 class AccountList(generics.ListAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializers
     
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['from_account', 'to_account','from_currency','to_currency']
+    
